d2c0b54dc271fb7bf0ba6a4ec8d93dcf59bc51df
ItsMe <itsme@ItsMes-MacBook-Pro.local>
2022-03-11 00:02:12 +0530

    Microservices Chapter 5

--- 6_build_all_by_one_command.sh
+++ 6_build_all_by_one_command.sh
@@ -3,7 +3,9 @@ set -o xtrace
 
 # 4. Now, we can build all the microservices with one command:
 
-./gradlew build -x test
+./gradlew build
+# If test are failing
+#./gradlew build -x test
 
 #> Run with --stacktrace option to get the stack trace.
 #> Run with --info or --debug option to get more log output.
