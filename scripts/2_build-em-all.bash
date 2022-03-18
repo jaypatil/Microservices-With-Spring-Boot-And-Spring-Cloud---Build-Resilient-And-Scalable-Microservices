9cbed8aea3335664fe36452124aeac2c0a30e833
jaypatil <jaypatil.sonu@gmail.com>
2022-03-18 21:31:15 +0530

    Build before making compose up

--- scripts/2_build-em-all.bash
+++ scripts/2_build-em-all.bash
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 set -o xtrace
 
-# 4. Now, we can build all the microservices with one command:
-./gradlew build --info
-#./gradlew build
-
-# turn up dataabase first so end to end test case should pass``
-docker-compose up -d
+# we can build all the microservices with one command:
+./gradlew build && docker-compose build && docker-compose up
